bd59f49e42398811eaddb99908146ce3899290b2
Rahulizthe <rahulrawat241371@gmail.com>
2026-07-10 22:38:57 +0530

    daily progress

--- c4_practice_set.py
+++ c4_practice_set.py
@@ -1,6 +1,6 @@
-#solution 1
+# #solution 1
 Fruits = []
-#taking inputs for 7 fruits.
+# # #taking inputs for 7 fruits.
 f1 = input("enter fruits name: ")
 Fruits.append(f1)
 f2 = input("enter fruits name: ")
@@ -16,3 +16,40 @@ Fruits.append(f6)
 f7 = input("enter fruits name: ")
 Fruits.append(f7)
 print(Fruits)
+
+# #solution 2
+marks = []
+# #input marks and sort them in ascending order.
+m1 = int(input("enter marks : "))
+marks.append(m1)
+m2 = int(input("enter marks : "))
+marks.append(m2)
+m3 = int(input("enter marks : "))
+marks.append(m3)
+m4 = int(input("enter marks : "))
+marks.append(m4)
+m5 = int(input("enter marks : "))
+marks.append(m5)
+m6 = int(input("enter marks : "))
+marks.append(m6)
+m7 = int(input("enter marks : "))
+marks.append(m7)
+marks.sort()
+
+# print(marks)
+
+#solution 3
+#proving tuple is immutable.
+tuple1 = ("apple",23,5.6,"hello")
+tuple1[2] =10 #not possible.
+
+#soltuion 4
+#program to sum all the item in a list.
+l = [23,12,3,43,34,7]
+#appraoch 1
+sum1 = l[0]+l[1]+l[2]+l[3]+l[4]+l[5]
+print(sum1)
+#appraoch 2
+print(sum(l))
+
+
